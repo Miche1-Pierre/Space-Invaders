dde284b2d4e73d665d387d84f171fa1802626815
Pi-et-Rho <pierre.michel.work@gmail.com>
2024-02-25 19:23:54 +0100

    Add obstacles

--- Engine.py
+++ Engine.py
@@ -1,18 +1,41 @@
 import pygame, sys
 from Settings import *
 from Player import *
+import Obstacle
 
 class Game:
     def __init__(self):
         player_sprite = Player((SCREEN_WIDTH / 2, SCREEN_HEIGHT - 10), SCREEN_WIDTH, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+        self.shape = Obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_position = [num * (SCREEN_WIDTH / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_position, x_start = SCREEN_WIDTH / 14, y_start = 480)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == "x":
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = Obstacle.Block(self.block_size, (241, 79, 80), x, y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
+    
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)        
 
-if __name__ == '__main__':  
+        self.blocks.draw(screen)
+
+if __name__ == '__main__':
 
     pygame.init()
 
---
+++ Obstacle.py
@@ -0,0 +1,18 @@
+import pygame
+
+shape = [
+    "  xxxxxxx",
+    " xxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxx     xxx",
+    "xx       xx"
+]
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, size, color, x, y):
+        super().__init__()
+        self.image = pygame.Surface((size, size))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(topleft = (x, y))
